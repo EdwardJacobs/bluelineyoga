9a1a959874dd0e4f95f91210b9dbf623b8baef2e
Edward Jacobs <eddielawrence23@gmail.com>
2020-04-18 16:27:55 -0500

    Add vailidations to models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ApplicationRecord
   has_many :lessons, dependent: :destroy
+
+  validates :name, presence: true
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,5 @@
 class Lesson < ApplicationRecord
   belongs_to :course
+
+  validates :name, presence: true
 end
